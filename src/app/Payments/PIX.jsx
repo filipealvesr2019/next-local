8ee9fafea8a62704b07155e01dcf06dcda81f233
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-10-07 19:33:57 -0300

    refactor: adiciona função de baixar qr code

--- src/app/Payments/PIX.jsx
+++ src/app/Payments/PIX.jsx
@@ -1,51 +1,103 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useConfig } from "../../../context/ConfigContext";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 import Cookies from "js-cookie";
-import ModalPix from '../components/ModalPix/ModalPix'
-export default function PIX(){
-    const [qrcode, setQrcode] = useState("");
+import ModalPix from "../components/ModalPix/ModalPix";
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from "@chakra-ui/react";
 
-    const { apiUrl } = useConfig();
-    const AdminID = Cookies.get("AdminID"); // Obtenha o ID do cliente do cookie
-    useEffect(() => {
-        async function getPix() {
-          try {
-            
-            const response = await axios.get(`${apiUrl}/api/pix/admin/${AdminID}`);
-            setQrcode(response.data);
-            console.log("qrcode", response.data);
-          } catch (error) {
-            console.error("Error fetching products:", error);
-            setQrcode(null);
-          }
-        }
+export default function PIX() {
+  const [qrcode, setQrcode] = useState("");
 
+  const { apiUrl } = useConfig();
+  const AdminID = Cookies.get("AdminID"); // Obtenha o ID do cliente do cookie
+  useEffect(() => {
+    async function getPix() {
+      try {
+        const response = await axios.get(`${apiUrl}/api/pix/admin/${AdminID}`);
+        setQrcode(response.data);
+        console.log("qrcode", response.data);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+        setQrcode(null);
+      }
+    }
 
-          getPix();
-      }, []);
+    getPix();
+  }, []);
 
+  return (
+    <>
 
+      <TableContainer
+        style={{
+          border: "1px solid #edf2f7",
+          borderRadius: "10px",
+          marginTop:"15rem",
+        }}
+      >
+              <ModalPix />
+        <Table variant="simple" style={{
+                    backgroundColor:"white"
 
-
-
-    return (
-        <>
-        <span style={{
-            marginTop:"15rem"
         }}>
-            <ModalPix />
-
+          <Thead>
+            <Tr>
+              <Th>QR Code</Th>
+              <Th>Chave Pix</Th>
+              <Th>Baixar QR Code</Th>
+              <Th>Excluir</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
             {qrcode && (
-        <>
-          {" "}
-          <img src={qrcode.qrCodeUrl} alt="" />
-          <span>{qrcode.pixKey}</span>
-        </>
-      )}
-        </span>
-        
-        </>
-    )
+              <>
+                <Tr>
+                  <Td>
+                    <img src={qrcode.qrCodeUrl} alt="" />
+                  </Td>
+                  <Td>
+                    <span>{qrcode.pixKey}</span>
+                  </Td>
+
+                  <Td>
+                  
+                  <a
+                    href={qrcode.qrCodeUrl}
+                    download="qrcode_pix.png" // Nome do arquivo a ser baixado
+                    style={{ width: "300px", height: "300px" }} // Aumenta o tamanho na tela
+
+                  >
+                    <button>Baixar QR Code</button>
+                  </a>
+                </Td>
+         
+                  <Td
+                    style={{
+                      color: "#C0392B",
+                      cursor: "pointer",
+                    }}
+                    onClick={() => openDeleteModal(revenue)}
+                  >
+                    <DeleteIcon />
+                  </Td>
+                </Tr>
+              </>
+            )}
+          </Tbody>
+        </Table>
+      </TableContainer>
+    </>
+  );
 }
