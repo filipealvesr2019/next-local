371fddc3330863577aa762909a7be375b205d333
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-10-12 21:42:55 -0300

    refactor: mostra horario de funcionamento

--- src/app/components/Horario/Horario.jsx
+++ src/app/components/Horario/Horario.jsx
@@ -25,7 +25,7 @@ import DeleteIcon from "@mui/icons-material/Delete";
 
 import { useConfig } from "../../../../context/ConfigContext";
 import HorarioModal from "./Modal/HorarioModal";
-import EditIcon from '@mui/icons-material/Edit';
+import EditIcon from "@mui/icons-material/Edit";
 export default function Horario() {
   const AdminID = Cookies.get("AdminID"); // Obtenha o ID do cliente do cookie
 
@@ -79,76 +79,92 @@ export default function Horario() {
         style={{
           display: "flex",
           flexDirection: "column",
- 
+
           // Habilita a rolagem apenas dentro dessa área
         }}
       >
         <div
           style={{
             marginTop: "10rem",
-          
           }}
-
         >
           <HorarioModal onSuccess={fetchProducts} />
           <TableContainer
-  style={{
-    border: "1px solid #edf2f7",
-    borderRadius: "10px",
-    maxHeight: "400px",
-    overflowY: "auto",
-    maxWidth: "80vw",
-  }}
->
-  <Table variant="simple" style={{ backgroundColor: "white" }}>
-    <Thead>
-      <Tr>
-        <Th p={2}>Segunda</Th>
-        <Th p={2}>Terça</Th>
-        <Th p={2}>Quarta</Th>
-        <Th p={2}>Quinta</Th>
-        <Th p={2}>Sexta</Th>
-        <Th p={2}>Sábado</Th>
-        <Th p={2}>Domingo</Th>
-        <Th p={2}>Excluir</Th>
-      </Tr>
-    </Thead>
-    <Tbody>
-      <Tr>
-        <Td p={2}>
-          {data?.segunda ? `${data.segunda.abertura} - ${data.segunda.fechamento}` : "Fechado"}
-        </Td>
-        <Td p={2}>
-          {data?.terca ? `${data.terca.abertura} - ${data.terca.fechamento}` : "Fechado"}
-        </Td>
-        <Td p={2}>
-          {data?.quarta ? `${data.quarta.abertura} - ${data.quarta.fechamento}` : "Fechado"}
-        </Td>
-        <Td p={2}>
-          {data?.quinta ? `${data.quinta.abertura} - ${data.quinta.fechamento}` : "Fechado"}
-        </Td>
-        <Td p={2}>
-          {data?.sexta ? `${data.sexta.abertura} - ${data.sexta.fechamento}` : "Fechado"}
-        </Td>
-        <Td p={2}>
-          {data?.sabado ? `${data.sabado.abertura} - ${data.sabado.fechamento}` : "Fechado"}
-        </Td>
-        
-        <Td p={2}>
-  {data?.domingo?.isOpen 
-    ? `${data.domingo.abertura} - ${data.domingo.fechamento}` 
-    : "Fechado"}
-</Td>
+            style={{
+              border: "1px solid #edf2f7",
+              borderRadius: "10px",
+              maxHeight: "400px",
+              overflowY: "auto",
+              maxWidth: "80vw",
+            }}
+          >
+            <Table variant="simple" style={{ backgroundColor: "white" }}>
+              <Thead>
+                <Tr>
+                  <Th p={2}>Segunda</Th>
+                  <Th p={2}>Terça</Th>
+                  <Th p={2}>Quarta</Th>
+                  <Th p={2}>Quinta</Th>
+                  <Th p={2}>Sexta</Th>
+                  <Th p={2}>Sábado</Th>
+                  <Th p={2}>Domingo</Th>
+                  <Th p={2}>Excluir</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                <Tr>
+                  <Td p={2}>
+                    {data?.segunda
+                      ? `${data.segunda.abertura} - ${data.segunda.fechamento}`
+                      : "Fechado"}
+                  </Td>
+                  <Td p={2}>
+                    {data?.terca
+                      ? `${data.terca.abertura} - ${data.terca.fechamento}`
+                      : "Fechado"}
+                  </Td>
+                  <Td p={2}>
+                    {data?.quarta
+                      ? `${data.quarta.abertura} - ${data.quarta.fechamento}`
+                      : "Fechado"}
+                  </Td>
+                  <Td p={2}>
+                    {data?.quinta
+                      ? `${data.quinta.abertura} - ${data.quinta.fechamento}`
+                      : "Fechado"}
+                  </Td>
+                  <Td p={2}>
+                    {data?.sexta
+                      ? `${data.sexta.abertura} - ${data.sexta.fechamento}`
+                      : "Fechado"}
+                  </Td>
+                  <Td p={2}>
+                    {data?.sabado
+                      ? `${data.sabado.abertura} - ${data.sabado.fechamento}`
+                      : "Fechado"}
+                  </Td>
 
-        <Td p={2} >
-        <EditIcon />
-          {data.length < 0 ? "" : <DeleteIcon style={{ color: "#C0392B", cursor: "pointer" }}  onClick={() => openDeleteModal()} />}
-        
-        </Td>
-      </Tr>
-    </Tbody>
-  </Table>
-</TableContainer>
+                  <Td p={2}>
+                    {data?.domingo?.isOpen
+                      ? `${data.domingo.abertura} - ${data.domingo.fechamento}`
+                      : "Fechado"}
+                  </Td>
+
+                  <Td p={2}>
+                    <EditIcon />
+                    {data.length < 0 ? (
+                      ""
+                    ) : (
+                      <DeleteIcon
+                        style={{ color: "#C0392B", cursor: "pointer" }}
+                        onClick={() => openDeleteModal()}
+                      />
+                    )}
+                  </Td>
+                </Tr>
+              </Tbody>
+            </Table>
+          </TableContainer>
 
           {/* Modal para confirmar a exclusão de despesa */}
           <Modal isOpen={isDeleteModalOpen} onClose={onCloseDeleteModal}>
