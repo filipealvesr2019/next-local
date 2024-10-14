7ff329a7feae3de85f2dea1a14b627585093c428
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-10-14 19:37:27 -0300

    refactor: adiciona verificação de campos necessarios

--- src/ecommerce/UserForm/UserForm.jsx
+++ src/ecommerce/UserForm/UserForm.jsx
@@ -1,11 +1,11 @@
-"use client"
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+"use client";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Cookies from "js-cookie";
-import { useAtom } from 'jotai';
-import { storeID } from '../../../store/store';
-import styles from "./SignUpForm.module.css"
-import { useConfig } from '../../../context/ConfigContext';
+import { useAtom } from "jotai";
+import { storeID } from "../../../store/store";
+import styles from "./SignUpForm.module.css";
+import { useConfig } from "../../../context/ConfigContext";
 
 const UserForm = () => {
   const { apiUrl } = useConfig();
@@ -13,31 +13,32 @@ const UserForm = () => {
   const UserID = Cookies.get("UserID"); // Obtenha o ID do cliente do cookie
   const storeNAME = Cookies.get("storeNAME"); // Obtenha o ID do cliente do cookie
 
-  console.log('storeIDAtom', storeIDAtom);
-  console.log('UserID', UserID);
+  console.log("storeIDAtom", storeIDAtom);
+  console.log("UserID", UserID);
   const [bairros, setBairros] = useState([]);
   const [formComplete, setFormComplete] = useState(false);
-  const [IsCepInvalid, setIsCepInvalid] = useState(false)
+  const [IsCepInvalid, setIsCepInvalid] = useState(false);
   const [bairroValido, setBairroValido] = useState(true);
-
-    const savedUserID = Cookies.get("UserID");
+  const [notificacaoVisivel, setNotificacaoVisivel] = useState(false);
+  const [erroVisivel, setErroVisivel] = useState(false);
+  const savedUserID = Cookies.get("UserID");
   const [showCEP, setShowCEP] = useState(false);
   const [formData, setFormData] = useState({
-    storeID: storeIDAtom, 
-    userID: UserID, 
-    name: '',
-    mobilePhone: '',
-    email: '',
-    postalCode: '',
-    address: '',
-    addressNumber: '',
-    complement: '',
-    province: '',
-    city: '',
-    state: '',
+    storeID: storeIDAtom,
+    userID: UserID,
+    name: "",
+    mobilePhone: "",
+    email: "",
+    postalCode: "",
+    address: "",
+    addressNumber: "",
+    complement: "",
+    province: "",
+    city: "",
+    state: "",
   });
 
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
 
   // Recuperar storeID e userID do cookie
   useEffect(() => {
@@ -46,13 +47,13 @@ const UserForm = () => {
 
     if (savedStoreID && !storeIDAtom) {
       setStoreID(savedStoreID);
-      setFormData(prevState => ({ ...prevState, storeID: savedStoreID }));
+      setFormData((prevState) => ({ ...prevState, storeID: savedStoreID }));
     } else if (storeIDAtom) {
-      setFormData(prevState => ({ ...prevState, storeID: storeIDAtom }));
+      setFormData((prevState) => ({ ...prevState, storeID: storeIDAtom }));
     }
 
     if (savedUserID) {
-      setFormData(prevState => ({ ...prevState, userID: savedUserID }));
+      setFormData((prevState) => ({ ...prevState, userID: savedUserID }));
     }
   }, [storeIDAtom, setStoreID]);
 
@@ -60,22 +61,22 @@ const UserForm = () => {
     const { name, value } = e.target;
     setFormData({
       ...formData,
-      [name]: value
+      [name]: value,
     });
-      // Verifica se todos os campos obrigatórios estão preenchidos
-      const requiredFields = [
-        "name",
-        "email",
-        "mobilePhone",
-        "postalCode",
-        "address",
-        "addressNumber",
-        "province",
-        "city",
-        "state",
-      ];
-      const isComplete = requiredFields.every((field) => formData[field]);
-      setFormComplete(isComplete);
+    // Verifica se todos os campos obrigatórios estão preenchidos
+    const requiredFields = [
+      "name",
+      "email",
+      "mobilePhone",
+      "postalCode",
+      "address",
+      "addressNumber",
+      "province",
+      "city",
+      "state",
+    ];
+    const isComplete = requiredFields.every((field) => formData[field]);
+    setFormComplete(isComplete);
   };
 
   const handleSubmit = async (e) => {
@@ -83,45 +84,58 @@ const UserForm = () => {
 
     // Certifique-se de que o userID e o storeID estão presentes antes de enviar o formulário
     if (!formData.userID || !formData.storeID) {
-      setMessage('Erro: ID de usuário ou loja ausente.');
+      setMessage("Erro: ID de usuário ou loja ausente.");
       return;
     }
 
     try {
       const response = await axios.post(`${apiUrl}/api/signupUser`, formData);
       setMessage(response.data.message);
+
+      // Exibe a notificação de sucesso
+      setNotificacaoVisivel(true);
+      setTimeout(() => {
+        setNotificacaoVisivel(false);
+      }, 3000);
     } catch (error) {
       if (error.response) {
         setMessage(error.response.data.message);
       } else {
-        setMessage('Erro ao criar usuário.');
+        setMessage("Erro ao criar usuário.");
       }
     }
   };
   const handleCepChange = async (event) => {
     const newCep = event.target.value.replace(/\D/g, ""); // Remove caracteres não numéricos
-      
+
     setFormData({ ...formData, postalCode: newCep });
-  
+
     if (newCep.length === 8) {
       try {
-        const response = await axios.get(`https://viacep.com.br/ws/${newCep}/json/`);
+        const response = await axios.get(
+          `https://viacep.com.br/ws/${newCep}/json/`
+        );
         const data = response.data;
         if (!data.erro) {
           // Verifica se o bairro, cidade e estado retornados correspondem
-          const bairroEncontrado = bairros.find(b => 
-            b.bairro.toLowerCase() === data.bairro.toLowerCase() &&
-            b.cidade.toLowerCase() === data.localidade.toLowerCase() &&
-            b.estado.toLowerCase() === data.uf.toLowerCase()
+          const bairroEncontrado = bairros.find(
+            (b) =>
+              b.bairro.toLowerCase() === data.bairro.toLowerCase() &&
+              b.cidade.toLowerCase() === data.localidade.toLowerCase() &&
+              b.estado.toLowerCase() === data.uf.toLowerCase()
           );
-          
+
           if (!bairroEncontrado) {
             setBairroValido(false);
             setShowCEP(false);
-            alert('Desculpe, ainda não cobrimos o seu bairro, cidade ou estado.');
+            // Exibe a notificação de erro
+            setErroVisivel(true);
+            setTimeout(() => {
+              setErroVisivel(false);
+            }, 3000);
           } else {
             setBairroValido(true);
-            setFormData(prevFormData => ({
+            setFormData((prevFormData) => ({
               ...prevFormData,
               address: data.logradouro,
               complement: data.complemento,
@@ -140,40 +154,68 @@ const UserForm = () => {
       }
     }
   };
-  
+
   useEffect(() => {
     const fetchEcommerce = async () => {
       if (!storeNAME) {
         console.error("storeNAME não encontrado no cookie.");
         return;
       }
-  
+
       try {
         const response = await axios.get(`${apiUrl}/api/loja/${storeNAME}`);
         const bairrosData = response.data.bairros; // Verifica se bairros está aqui
-        
+
         if (Array.isArray(bairrosData)) {
           setBairros(bairrosData);
         } else {
           console.error("Bairros não é uma lista:", bairrosData);
           setBairros([]); // Defina como lista vazia se não for um array
         }
-  
       } catch (error) {
         console.error("Erro ao buscar o e-commerce:", error);
       }
     };
-  
+
     fetchEcommerce();
   }, [apiUrl, storeNAME]);
-  
+
   return (
     <div>
+      {notificacaoVisivel && (
+        <div style={stylesNotificacao.notificacao}>{message}</div>
+      )}
+      {erroVisivel && (
+        <div style={stylesNotificacao.erro}>
+          Desculpe, ainda não cobrimos o seu bairro, cidade ou estado.
+        </div>
+      )}
       <h2>Cadastro de Usuário</h2>
       <form onSubmit={handleSubmit}>
-        <input type="text" name="name" placeholder="Nome" onChange={handleChange} value={formData.name} required />
-        <input type="text" name="mobilePhone" placeholder="Telefone" onChange={handleChange} value={formData.mobilePhone} required />
-        <input type="email" name="email" placeholder="Email" onChange={handleChange} value={formData.email} required />
+        <input
+          type="text"
+          name="name"
+          placeholder="Nome"
+          onChange={handleChange}
+          value={formData.name}
+          required
+        />
+        <input
+          type="text"
+          name="mobilePhone"
+          placeholder="Telefone"
+          onChange={handleChange}
+          value={formData.mobilePhone}
+          required
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          onChange={handleChange}
+          value={formData.email}
+          required
+        />
         <input
           type="text"
           name="postalCode"
@@ -182,16 +224,59 @@ const UserForm = () => {
           placeholder="77777-777"
           className={styles.input}
         />
-        <button type="submit" disabled={!formComplete} style={{ backgroundColor: formComplete ? "#05070A" : "#ccc" }}>Cadastrar</button>
+        <button
+          type="submit"
+          disabled={!formComplete}
+          style={{ backgroundColor: formComplete ? "#05070A" : "#ccc" }}
+        >
+          Cadastrar
+        </button>
       </form>
       {showCEP && (
         <>
-          <input type="text" name="address" placeholder="Endereço" onChange={handleChange} value={formData.address} />
-          <input type="text" name="addressNumber" placeholder="Número" onChange={handleChange} value={formData.addressNumber} required />
-          <input type="text" name="complement" placeholder="Complemento" onChange={handleChange} value={formData.complement} />
-          <input type="text" name="province" placeholder="Bairro" onChange={handleChange} value={formData.province} />
-          <input type="text" name="city" placeholder="Cidade" onChange={handleChange} value={formData.city} />
-          <input type="text" name="state" placeholder="Estado" onChange={handleChange} value={formData.state} />
+          <input
+            type="text"
+            name="address"
+            placeholder="Endereço"
+            onChange={handleChange}
+            value={formData.address}
+          />
+          <input
+            type="text"
+            name="addressNumber"
+            placeholder="Número"
+            onChange={handleChange}
+            value={formData.addressNumber}
+            required
+          />
+          <input
+            type="text"
+            name="complement"
+            placeholder="Complemento"
+            onChange={handleChange}
+            value={formData.complement}
+          />
+          <input
+            type="text"
+            name="province"
+            placeholder="Bairro"
+            onChange={handleChange}
+            value={formData.province}
+          />
+          <input
+            type="text"
+            name="city"
+            placeholder="Cidade"
+            onChange={handleChange}
+            value={formData.city}
+          />
+          <input
+            type="text"
+            name="state"
+            placeholder="Estado"
+            onChange={handleChange}
+            value={formData.state}
+          />
         </>
       )}
       {message && <p>{message}</p>}
@@ -199,4 +284,27 @@ const UserForm = () => {
   );
 };
 
+// Estilos inline para a notificação e erro
+const stylesNotificacao = {
+  notificacao: {
+    position: "fixed",
+    bottom: "20px",
+    right: "20px",
+    padding: "10px 20px",
+    backgroundColor: "#4CAF50",
+    color: "white",
+    borderRadius: "5px",
+    boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.1)",
+  },
+  erro: {
+    position: "fixed",
+    bottom: "20px",
+    right: "20px",
+    padding: "10px 20px",
+    backgroundColor: "#f44336",
+    color: "white",
+    borderRadius: "5px",
+    boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.1)",
+  },
+};
 export default UserForm;
