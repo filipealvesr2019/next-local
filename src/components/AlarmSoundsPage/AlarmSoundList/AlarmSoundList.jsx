9a9f57ea06b10ec44ef7c6c750254e678090b820
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-10-15 18:08:45 -0300

    refactor: adiciona alarme

--- src/components/AlarmSoundsPage/AlarmSoundList/AlarmSoundList.jsx
+++ src/components/AlarmSoundsPage/AlarmSoundList/AlarmSoundList.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 const AlarmSoundList = ({ sounds, onPlay, selectedAlarm, isAlarmActive }) => {
   return (
     <ul>
       {sounds.map((sound, index) => (
         <li key={index}>
           <span>{sound}</span>
-          <button onClick={() => onPlay(sound)}>Tocar</button>
+          <PlayArrowIcon onClick={() => onPlay(sound)} />
           {selectedAlarm === sound && <span> (Selecionado)</span>}
         </li>
       ))}
